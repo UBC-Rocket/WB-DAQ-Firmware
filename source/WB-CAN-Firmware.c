86d814f16fdc7ae843f9f1f71576053878e86d28
Davin Birdi <davin.birdi@alumni.ubc.ca>
2021-12-09 20:45:36 -0800

    Copied over SDK Example into DAQ Project, buildss successfully

--- source/WB-CAN-Firmware.c
+++ source/WB-CAN-Firmware.c
@@ -19,6 +19,7 @@
 #include "fsl_dspi.h"
 #include "fsl_dspi_freertos.h"
 #include "fsl_gpio.h"
+#include "fsl_adc16.h"
 
 /*******************************************************************************
  * Prototypes
@@ -26,6 +27,31 @@
 static void blinkTask(void *pv);
 static void testTask(void *pv);
 static void actuatorTask(void *pv);
+static void ADCTask(void *pv);
+
+// ADC Interrupt:
+void DEMO_ADC16_IRQ_HANDLER_FUNC(void);
+
+
+
+/*******************************************************************************
+ * ADC Global Variables From Example - Please do non-global after verification
+ ******************************************************************************/
+volatile bool g_Adc16ConversionDoneFlag = false;
+volatile uint32_t g_Adc16ConversionValue;
+volatile uint32_t g_Adc16InterruptCounter;
+const uint32_t g_Adc16_12bitFullRange = 4096U;
+
+#define DEMO_ADC16_BASE          ADC1
+#define DEMO_ADC16_CHANNEL_GROUP 0U
+#define DEMO_ADC16_USER_CHANNEL  23U
+
+#define DEMO_ADC16_IRQn             ADC1_IRQn
+#define DEMO_ADC16_IRQ_HANDLER_FUNC ADC1_IRQHandler
+
+
+
+
 
 /*******************************************************************************
  * Main
@@ -82,6 +108,17 @@ int main(void) {
     	    ;
     };
 
+    if ((error =  xTaskCreate(ADCTask,
+        "ADC Task",
+    	1024,
+    	NULL,
+    	0,
+    	NULL)) != pdPASS) {
+        	printf("ADC Task init failed: %ld\n", error);
+        	for (;;)
+        	    ;
+        };
+
     vTaskStartScheduler();
 
     for(;;);
@@ -104,8 +141,6 @@ static void testTask(void *pv) {
 	}
 }
 
-
-
 static void actuatorTask(void *pv){
 	for(;;){
 		// Toggle indefinitely to show they work, will add control loop later
@@ -113,5 +148,62 @@ static void actuatorTask(void *pv){
 		GPIO_PortToggle(BOARD_INITPINS_HS_SWITCH_B_IN1_GPIO, BOARD_INITPINS_HS_SWITCH_B_IN1_GPIO_PIN_MASK);
 		vTaskDelay(pdMS_TO_TICKS(200));
 	}
+}
+
+
+
+static void ADCTask(void *pv) {
+	while(1) {
+		vTaskDelay(pdMS_TO_TICKS(500));
+			adc16_config_t adc16ConfigStruct;
+		    adc16_channel_config_t adc16ChannelConfigStruct;
+
+		    BOARD_InitPins();
+		    BOARD_BootClockRUN();
+		    BOARD_InitDebugConsole();
+		    EnableIRQ(DEMO_ADC16_IRQn);
+
+		    PRINTF("\r\nADC16 interrupt Example.\r\n");
+
+		    ADC16_GetDefaultConfig(&adc16ConfigStruct);
+
+		    PRINTF("ADC Full Range: %d\r\n", g_Adc16_12bitFullRange);
+		    PRINTF("Press any key to get user channel's ADC value ...\r\n");
+
+		    adc16ChannelConfigStruct.channelNumber                        = DEMO_ADC16_USER_CHANNEL;
+		    adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true; /* Enable the interrupt. */
+
+		    g_Adc16InterruptCounter = 0U;
+
+		    while (1)
+		    {
+		        GETCHAR();
+		        g_Adc16ConversionDoneFlag = false;
+		        /*
+		         When in software trigger mode, each conversion would be launched once calling the "ADC16_ChannelConfigure()"
+		         function, which works like writing a conversion command and executing it. For another channel's conversion,
+		         just to change the "channelNumber" field in channel configuration structure, and call the function
+		         "ADC16_ChannelConfigure()"" again.
+		         Also, the "enableInterruptOnConversionCompleted" inside the channel configuration structure is a parameter for
+		         the conversion command. It takes affect just for the current conversion. If the interrupt is still required
+		         for the following conversion, it is necessary to assert the "enableInterruptOnConversionCompleted" every time
+		         for each command.
+		        */
+		        ADC16_SetChannelConfig(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP, &adc16ChannelConfigStruct);
+		        while (!g_Adc16ConversionDoneFlag)
+		        {
+		        }
+		        PRINTF("ADC Value: %d\r\n", g_Adc16ConversionValue);
+		        PRINTF("ADC Interrupt Count: %d\r\n", g_Adc16InterruptCounter);
+		    }
+	}
+}
+void DEMO_ADC16_IRQ_HANDLER_FUNC(void)
+{
+    g_Adc16ConversionDoneFlag = true;
+    /* Read conversion result to clear the conversion completed flag. */
+    g_Adc16ConversionValue = ADC16_GetChannelConversionValue(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP);
+    g_Adc16InterruptCounter++;
+    SDK_ISR_EXIT_BARRIER;
 }
 
